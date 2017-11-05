5d511dd03c9b47a46b736f1fd0413b515b828a38
Michael Wilson <info@michaelwilsion.io>
2017-11-04 22:51:54 -0400

    Created custom html attributes for calculator buttons

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -2,3 +2,9 @@
 
 const display = document.querySelector("#display span");
 
+
+// ---- Check if display has anything inputted, if not, then display a "blinking" underscore
+const blinker = `<span id="waiting">_</span>`;
+if ( display.textContent === "" ) {
+    display.innerHTML = blinker;
+}

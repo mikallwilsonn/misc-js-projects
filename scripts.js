5d6592219f005286b6f5ccb97f52b58aa2efc1f4
Michael Wilson <info@michaelwilsion.io>
2017-11-27 11:01:29 -0500

    links of project feature now open in a new minimal window instead of leaving the main page

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 // -------- Project Feature Scripts
-// ---- ( if there are or will be any... )
+
+const links = document.querySelectorAll( '.project a.project-link' );
+
+links.forEach( link => {
+    let dest = link.href;
+    
+    link.addEventListener('click', function( e ) {
+        e.preventDefault();
+        console.log( 'You are being taken to ' + dest );
+        window.open( dest, 'newwindow', 'width=1500, height=1000' );
+        return false;
+    })
+});
